fde1c9979d15890dd35a1390eefd1500a6badd14
Arjun <nagarjuna.mamidala@gmail.com>
2022-05-23 17:36:44 +0530

    Code Changes

---
+++ Dummy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while read $a
+do 
+	echo $a
+done < $1
+
--- stringreverse.sh
+++ stringreverse.sh
@@ -2,7 +2,7 @@
 n=`echo $1 | wc -c`
 while [ $n -gt 0 ]
 do
-  t=$t`echo $1 | cut -c"$n"p`
+  t=$t`echo $1 | cut -c"$n"`
   n=`expr $n - 1`
 done
 echo $t
